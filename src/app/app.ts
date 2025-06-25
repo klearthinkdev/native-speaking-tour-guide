6b5b0c20d347e33a30696807aa5cd4d43f2a2eab
klearthinkdev <dev@klearthink.com>
2025-06-25 12:27:31 +0800

    @angular/material & themes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ThemeService } from './shared/services/theme.service';
+import { Theme } from './shared/enums/theme.enum';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +11,18 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected title = 'native-speaking-tour-guide';
+
+  constructor(public th: ThemeService) {
+    this.th.init();
+  }
+
+  useNextTheme(): void {
+    const all = [Theme.Light, Theme.Dark, Theme.System];
+    const currentIndex = all.indexOf(this.th.currentTheme);
+    const next = all[(currentIndex + 1) % all.length];
+
+    console.log(currentIndex, next);
+
+    this.th.use(next);
+  }
 }
---
+++ src/app/shared/services/theme.service.ts
@@ -0,0 +1,92 @@
+import { DOCUMENT, Inject, Injectable } from '@angular/core';
+import { Theme } from '../enums/theme.enum';
+import { BehaviorSubject, distinctUntilChanged, Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ThemeService {
+  private _localStorageKey = 'theme';
+  private _defaultTheme = Theme.System;
+  private _initialized = false;
+
+  themeChange = new Subject<Theme>();
+  isDark$ = new BehaviorSubject<boolean>(false);
+
+  currentTheme!: Theme;
+
+  get isDark() {
+    return this.isDark$.getValue();
+  }
+  set isDark(value) {
+    this.isDark$.next(value);
+  }
+
+  constructor(
+    @Inject(DOCUMENT)
+    private document: Document & { startViewTransition: Function },
+  ) {}
+
+  init(): void {
+    if (this._initialized) {
+      return;
+    }
+
+    this.themeChange.pipe(distinctUntilChanged()).subscribe((theme) => {
+      if (this.validateTheme(theme)) {
+        this.onToggleClass(theme);
+
+        this.saveTheme(theme);
+
+        this.currentTheme = theme;
+      }
+    });
+
+    const _theme = this.loadTheme();
+    const defaultTheme = this.validateTheme(_theme)
+      ? (_theme as Theme)
+      : this._defaultTheme;
+
+    this.use(defaultTheme);
+
+    this._initialized = true;
+  }
+
+  use(theme: Theme): void {
+    this.themeChange.next(theme);
+  }
+
+  private loadTheme(): string | null {
+    return localStorage.getItem(this._localStorageKey);
+  }
+
+  private saveTheme(theme: string): void {
+    localStorage.setItem(this._localStorageKey, theme);
+  }
+
+  private onToggleClass(theme: Theme) {
+    const isDark =
+      theme === Theme.Dark ||
+      (theme === Theme.System &&
+        this.document.defaultView?.matchMedia('(prefers-color-scheme: dark)')
+          .matches);
+
+    if (this.document.startViewTransition === undefined) {
+      this.toggleClass(isDark);
+    } else {
+      this.document.startViewTransition(() => {
+        this.toggleClass(isDark);
+      });
+    }
+
+    this.isDark$.next(isDark === true);
+  }
+
+  private toggleClass(isDark?: boolean) {
+    this.document.documentElement.classList[isDark ? 'add' : 'remove']('dark');
+  }
+
+  private validateTheme(theme: string | null): boolean {
+    return Object.values(Theme).includes(theme as Theme);
+  }
+}
